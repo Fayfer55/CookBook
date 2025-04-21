4ea87d82a6479085aeba3926b82e3fd2a6a8f521
Kirill Faifer <sayfer555@gmail.com>
2025-04-21 16:36:35 +0600

    added IngredientCategory enum

---
+++ CookBook/Entities/IngredientCategory.swift
@@ -0,0 +1,20 @@
+//
+//  IngredientCategory.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 21.04.2025.
+//
+
+import Foundation
+
+public enum IngredientCategory: String, Codable, CaseIterable {
+    case spicesSeasonings
+    case vegetables
+    case fruits
+    case dairy
+    case proteins
+    case grains
+    case pasta
+    case oils
+    case vinegars
+}

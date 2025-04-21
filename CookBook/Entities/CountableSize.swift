f76af72c7f0994cf7a4f4be9ca380c9c1d3da1ec
Kirill Faifer <sayfer555@gmail.com>
2025-04-21 15:00:33 +0600

    create IngredientUnit enum & CoutableSize enum

---
+++ CookBook/Entities/CountableSize.swift
@@ -0,0 +1,14 @@
+//
+//  CountableSize.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 21.04.2025.
+//
+
+import Foundation
+
+enum CountableSize: String, Codable {
+    case small
+    case medium
+    case large
+}
---
+++ CookBook/Entities/IngredientUnit.swift
@@ -0,0 +1,16 @@
+//
+//  IngredientUnit.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 21.04.2025.
+//
+
+import Foundation
+
+enum IngredientUnit: String, Codable {
+    case gram
+    case teaspoon
+    case tablespoon
+    case cup
+    case piece  // For countable ingredients like "1 tomato"
+}

0a6446ed84ce1cad15815ec6e49409c8dd800f1d
Kirill Faifer <sayfer555@gmail.com>
2025-04-25 16:41:09 +0600

    make RecipeListViewController inherited of UITableViewController with FetchedResultController implementation

--- CookBook/Modules/RecipeList/RecipeListViewController.swift
+++ CookBook/Modules/RecipeList/RecipeListViewController.swift
@@ -6,17 +6,89 @@
 //
 
 import UIKit
+import CoreData
 
-final class RecipeListViewController: UIViewController {
+final class RecipeListViewController: UITableViewController {
+    
+    // MARK: - Proeprties
+    
+    private let context: NSManagedObjectContext
+    
+    private var request = Recipe.fetchRequest()
+    
+    private lazy var fetchedResultController: NSFetchedResultsController<Recipe> = {
+        let controller = NSFetchedResultsController(
+            fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil
+        )
+        controller.delegate = self
+        return controller
+    }()
+    
+    private lazy var dataSource = UITableViewDiffableDataSource<Int, NSManagedObjectID>(tableView: tableView) { [unowned self] tableView, indexPath, objectID in
+        guard let recipe = try? self.context.existingObject(with: objectID) as? Recipe else {
+            fatalError("Managed object should exist")
+        }
+        let cell: RecipeListTableCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.configure(with: recipe.title, subtitle: recipe.subtitle)
+        return cell
+    }
     
     // MARK: - Lifecycle
-
-    override func loadView() {
-        view = RecipeListView()
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        super.init(style: .grouped)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupParentView()
+        performCoreDataRequest()
+    }
+    
+    // MARK: - Layout
+    
+    private func setupParentView() {
+        tableView.register(cellType: RecipeListTableCell.self)
+    }
+    
+    private func performCoreDataRequest() {
+        do {
+            request.sortDescriptors = []
+            try fetchedResultController.performFetch()
+        } catch {
+            print(error)
+        }
     }
     
 }
 
-#Preview {
-    RecipeListViewController()
+// MARK: - NSFetchedResultsControllerDelegate
+
+nonisolated
+extension RecipeListViewController: NSFetchedResultsControllerDelegate {
+    
+    func controller(
+        _ controller: NSFetchedResultsController<any NSFetchRequestResult>,
+        didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference
+    ) {
+        let snapshot = snapshot as NSDiffableDataSourceSnapshot<Int, NSManagedObjectID>
+        
+        print(snapshot.itemIdentifiers.count)
+        
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+    
 }
+
+//#Preview {
+//    RecipeListViewController()
+//}

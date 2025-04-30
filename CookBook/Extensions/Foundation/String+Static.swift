10c49c610fe1c0fffef1adadf5898eb58c77a19f
Kirill Faifer <sayfer555@gmail.com>
2025-04-30 14:46:11 +0600

    add String+Static extension

---
+++ CookBook/Extensions/Foundation/String+Static.swift
@@ -0,0 +1,16 @@
+//
+//  String+Static.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 30.04.2025.
+//
+
+import Foundation
+
+extension String {
+    
+    static let empty = ""
+    static let dot = "."
+    static let space = " "
+    
+}

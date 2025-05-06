7c1c45380cff62e38a32154a54dc09bae2b0b3f0
Kirill Faifer <sayfer555@gmail.com>
2025-05-06 15:19:31 +0600

    create DataFetchable protocol

---
+++ CookBook/Protocols/DataFetchable.swift
@@ -0,0 +1,18 @@
+//
+//  DataFetchable.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 02.05.2025.
+//
+
+import UIKit
+
+/// This is protocol for fetching array of Items. It uses NSDiffableDataSource as an abstraction layer of storing.
+/// It allows to combine CoreData's NSFetchedResultsControllerDelegate & some mock's type to show in UITableViewController
+protocol DataFetchable<SectionIdentifierType, ItemIdentifierType> {
+    associatedtype SectionIdentifierType: Hashable, Sendable
+    associatedtype ItemIdentifierType: Hashable, Sendable
+    
+    var delegate: (any DiffableDataSourceFetchDelegate<SectionIdentifierType, ItemIdentifierType>)? { get set }
+    func fetchRequest()
+}

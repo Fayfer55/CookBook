4b0481a5112d677829b37fc3d7319553ee721979
Kirill Faifer <sayfer555@gmail.com>
2025-06-23 14:37:23 +0600

    add Pantry module

---
+++ CookBook/Modules/Pantry/PantryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PantryViewController.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 23.06.2025.
+//
+
+import UIKit
+
+final class PantryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

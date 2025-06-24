e89c1716d37b06aad290f7cf866288e899fd9516
Kirill Faifer <sayfer555@gmail.com>
2025-06-24 10:58:18 +0600

    add TextFieldCollectionCell

---
+++ CookBook/Base/Cells/TextFieldCollectionCell.swift
@@ -0,0 +1,41 @@
+//
+//  TextFieldCollectionCell.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 23.06.2025.
+//
+
+import UIKit
+
+class TextFieldCollectionCell: UICollectionViewCell, ClassIdentifiable {
+    
+    lazy var textField = UITextField()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupSubviews()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    
+    func setupSubviews() {
+        contentView.addSubview(textField)
+        
+        makeConstrainsts()
+    }
+    
+    func makeConstrainsts() {
+        textField.snp.makeConstraints { make in
+            make.directionalEdges.equalToSuperview()
+        }
+    }
+    
+}

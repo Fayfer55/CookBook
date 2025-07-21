99859fe3afd13bb853f4c54ca9480c045455ad30
Kirill Faifer <sayfer555@gmail.com>
2025-07-21 19:39:47 +0600

    make RecipeCreationVC contains only controller logic and presenter

--- CookBook/Modules/RecipeCreation/RecipeCreationViewController.swift
+++ CookBook/Modules/RecipeCreation/RecipeCreationViewController.swift
@@ -9,54 +9,11 @@ import UIKit
 
 final class RecipeCreationViewController: UIViewController {
     
-    private let viewModel: RecipeCreationInterface
-    
-    // MARK: - UI Elements
-    
-    private lazy var scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        return scrollView
-    }()
-    
-    private lazy var nameTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Recipe name"
-        textField.layer.cornerRadius = 15
-        textField.layer.borderWidth = 3
-        textField.layer.borderColor = UIColor.secondarySystemBackground.cgColor
-        textField.delegate = self
-        textField.returnKeyType = .done
-        return textField
-    }()
-    
-    private lazy var ingredientsCollection = IngredientCollectionViewController()
-    
-    private lazy var instructionViewController = InstructionListViewController()
-    
-    private lazy var addStepButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setTitle("Step", for: .normal)
-        button.setImage(UIImage(systemName: "plus.circle"), for: .normal)
-        button.configuration = .borderless()
-        button.addTarget(self, action: #selector(addStep), for: .touchUpInside)
-        button.directionalLayoutMargins = .safeArea
-        return button
-    }()
-    
-    private lazy var createButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.setTitle("Create", for: .normal)
-        button.directionalLayoutMargins = .safeArea
-        button.configuration = .filled()
-        button.configuration?.cornerStyle = .large
-        button.addTarget(self, action: #selector(createRecipe), for: .touchUpInside)
-        return button
-    }()
+    private lazy var presenter = RecipeCreationPresenter(view: self)
     
     // MARK: - Lifecycle
     
-    init(model: RecipeCreationInterface) {
-        viewModel = model
+    init() {
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -64,114 +21,16 @@ final class RecipeCreationViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("method unavailable")
     }
+    
+    override func loadView() {
+        view = RecipeCreationView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.title = "Recipe"
         view.backgroundColor = .systemBackground
-        setupSubviews()
-        
-        ingredientsCollection.set(ingredients: viewModel.allIngredients())
-    }
-    
-    // MARK: - Layout
-    
-    private func setupSubviews() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(nameTextField)
-        
-        addChild(ingredientsCollection)
-        addChild(instructionViewController)
-        
-        scrollView.addSubview(ingredientsCollection.view)
-        scrollView.addSubview(instructionViewController.view)
-        scrollView.addSubview(addStepButton)
-        view.addSubview(createButton)
-        
-        makeConstraints()
-        
-        ingredientsCollection.didMove(toParent: self)
-        instructionViewController.didMove(toParent: self)
-    }
-    
-    private func makeConstraints() {
-        scrollView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.leading.trailing.equalToSuperview()
-            $0.bottom.equalTo(createButton.snp.top)
-        }
-        scrollView.contentLayoutGuide.snp.makeConstraints {
-            $0.width.equalTo(view.snp.width)
-        }
-        nameTextField.snp.makeConstraints {
-            $0.top.equalTo(scrollView.contentLayoutGuide.snp.top).offset(20)
-            $0.height.equalTo(50)
-            $0.leading.trailing.equalTo(scrollView.contentLayoutGuide).inset(16)
-        }
-        ingredientsCollection.view.snp.makeConstraints {
-            $0.top.equalTo(nameTextField.snp.bottom).offset(30)
-            $0.leading.trailing.equalTo(scrollView.contentLayoutGuide).inset(16)
-        }
-        addStepButton.snp.makeConstraints {
-            $0.top.equalTo(ingredientsCollection.view.snp.bottom).offset(10)
-            $0.centerX.equalToSuperview()
-        }
-        instructionViewController.view.snp.makeConstraints {
-            $0.top.equalTo(addStepButton.snp.bottom).offset(20)
-            $0.leading.trailing.equalTo(scrollView.contentLayoutGuide).inset(16)
-            $0.bottom.equalTo(scrollView.contentLayoutGuide.snp.bottom)
-        }
-        createButton.snp.makeConstraints {
-            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
-            $0.height.equalTo(50)
-            $0.leading.trailing.equalToSuperview().inset(16)
-        }
-    }
-    
-    // MARK: - Actions
-    
-    @objc
-    private func createRecipe() {
-        viewModel.recipe.title = nameTextField.text ?? "Some recipe"
-        viewModel.recipe.addToIngredients(NSSet(set: ingredientsCollection.selectedIngredients))
-        
-        do {
-            try viewModel.saveRecipe()
-            navigationController?.popViewController(animated: true)
-        } catch {
-            print(error)
-        }
-    }
-    
-    @objc private func addStep() {
-        let alertController = UIAlertController(title: "Write a step instruction", message: nil, preferredStyle: .alert)
-        alertController.addTextField()
-        
-        let addAction = UIAlertAction(title: "Add", style: .default) { [unowned self] _ in
-            guard let textField = alertController.textFields?.first, let text = textField.text, !text.isEmpty else { return }
-            let step = viewModel.nextCookStep()
-            step.title = text
-            
-            viewModel.recipe.addToInstruction(step)
-            self.instructionViewController.add(step: step)
-        }
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        alertController.addAction(addAction)
-        alertController.addAction(cancelAction)
-        
-        navigationController?.present(alertController, animated: true)
-    }
-    
-}
-
-// MARK: - UITextFieldDelegate
-
-extension RecipeCreationViewController: UITextFieldDelegate {
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
     }
     
 }

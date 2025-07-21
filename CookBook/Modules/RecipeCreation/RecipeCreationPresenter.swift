1da136d0e597ee671c2e9f92c1623b81e047130a
Kirill Faifer <sayfer555@gmail.com>
2025-07-21 17:31:13 +0600

    add almost empty RecipeCreationPresenter

---
+++ CookBook/Modules/RecipeCreation/RecipeCreationPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeCreationPresenter.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 21.07.2025.
+//
+
+import Foundation
+
+struct RecipeCreationPresenter {
+    
+    unowned let view: RecipeCreationViewController
+    
+}

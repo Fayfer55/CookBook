157adf9bf61b2999f3134d55087194b6fff23817
Kirill Faifer <sayfer555@gmail.com>
2025-07-21 19:57:16 +0600

    add almost empty IngredientsPickPresenter

---
+++ CookBook/Modules/IngredientPick/IngredientsPickPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  IngredientsPickPresenter.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 21.07.2025.
+//
+
+import Foundation
+
+struct IngredientsPickPresenter {
+    
+    unowned let view: IngredientsPickViewController
+    
+}

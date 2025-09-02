f2c25eae08f52b4db148f8016390e8431c6ba42e
Kirill Faifer <sayfer555@gmail.com>
2025-09-02 13:43:08 +0600

    add IngredientsListPresenter

---
+++ CookBook/Modules/ButtonedTextFieldList/IngredientsListPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  IngredientsListPresenter.swift
+//  CookBook
+//
+//  Created by Kirill Faifer on 02.09.2025.
+//
+
+import Foundation
+
+final class IngredientsListPresenter {
+    
+    var ingredients = [Ingredient]()
+    
+}

03bd62e6fdffc9b40626cef5fde9d239f8c6c5cd
Kirill Faifer <sayfer555@gmail.com>
2025-09-02 13:50:54 +0600

    improve code logic in IngredientsListVC

--- CookBook/Modules/ButtonedTextFieldList/IngredientsListViewController.swift
+++ CookBook/Modules/ButtonedTextFieldList/IngredientsListViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class IngredientsListViewController: UITableViewController {
     
     var textFieldDidChange: ((_ textField: UITextField) -> Void)?
+    var textFieldDidReturn: ((_ textField: UITextField) -> Void)?
+    
+    private lazy var presenter = IngredientsListPresenter()
     
     private lazy var dataSource = UITableViewDiffableDataSource<Int, Int>(tableView: tableView) { [unowned self] tableView, indexPath, _ in
         let cell: ButtonedTextFieldListCell = tableView.dequeueReusableCell(for: indexPath)
         cell.textField.placeholder = "New item"
         cell.textField.delegate = self
+        cell.textField.autocorrectionType = .no
+        cell.textField.tag = indexPath.row
+        cell.textField.returnKeyType = .next
         cell.textField.addTarget(self, action: #selector(textFieldDidChangeEditing), for: .editingChanged)
-        if indexPath.row != .zero { // TODO: - improve logic
-            cell.textField.becomeFirstResponder()
-        }
         return cell
     }
 
@@ -46,6 +49,33 @@ class IngredientsListViewController: UITableViewController {
     private func textFieldDidChangeEditing(_ textField: UITextField) {
         textFieldDidChange?(textField)
     }
+    
+    // MARK: - Helpers
+    
+    func addIngredient(_ ingredient: Ingredient, place: Int) {
+        if presenter.ingredients.indices.contains(place) {
+            presenter.ingredients[place] = ingredient
+        } else {
+            presenter.ingredients.append(ingredient)
+        }
+    }
+    
+    func makeNextRowFirstResponder(currentRow: Int) {
+        let itemsCount = tableView.numberOfRows(inSection: .zero)
+        
+        if currentRow == itemsCount - 1 {
+            createNextRow()
+        }
+        
+        let cell: ButtonedTextFieldListCell = tableView.cellForRow(at: IndexPath(row: currentRow + 1, section: .zero))
+        cell.textField.becomeFirstResponder()
+    }
+    
+    private func createNextRow() {
+        var snapshot = dataSource.snapshot()
+        snapshot.appendItems([snapshot.numberOfItems(inSection: .zero)], toSection: .zero)
+        dataSource.apply(snapshot)
+    }
 
 }
 
@@ -54,11 +84,7 @@ class IngredientsListViewController: UITableViewController {
 extension IngredientsListViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        var snapshot = dataSource.snapshot()
-        let itemsCount = snapshot.numberOfItems(inSection: .zero)
-        snapshot.appendItems([itemsCount], toSection: .zero)
-        dataSource.apply(snapshot)
-        
+        textFieldDidReturn?(textField)
         return true
     }
     
